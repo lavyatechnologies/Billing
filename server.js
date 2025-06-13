0ba50a1ebc9f63759d42524d0c558e9cf88a117a
lavyatechnologies <73527447+lavyatechnologies@users.noreply.github.com>
2025-06-13 15:38:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-
+const PORT = process.env.PORT ;
 
 // ✅ Ensure 'uploads' folder exists
 const uploadDir = path.join(__dirname, "uploads");
